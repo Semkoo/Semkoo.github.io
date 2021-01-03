f04084cc37d50b7d1b25f1492115a125d6b57b53
Semkoo <semirnik2@gmail.com>
2021-01-03 15:08:41 -0500

    Reverd to dist

--- scripts/clean.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-const sh = require("shelljs");
-const path = require("path");
-
-const destPath = path.resolve(path.dirname(__filename), "../gh-pages");
-
-sh.rm("-rf", `${destPath}/*`);
